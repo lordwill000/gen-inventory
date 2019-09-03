854b4cacb6c6d3b68b213e15f25e019524386bb1
Lordwill Mabalot <lordwill.mabalot@gmail.com>
2019-09-03 19:45:22 +0800

    :heavy_plus_sign: Added bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import '@/assets/scss/main.scss';
 import App from '@/App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
