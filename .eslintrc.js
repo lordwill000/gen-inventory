6ee5ec91ab62044e14401e5fb845a5c5d2e2b1e7
Lordwill Mabalot <lordwill.mabalot@gmail.com>
2019-09-05 20:18:53 +0800

    :wrench: Added vue formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,50 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "global-require": 0
+    "global-require": 0,
+    'vue/attributes-order' : ["error", {
+      "order": [
+        "DEFINITION",
+        "LIST_RENDERING",
+        "CONDITIONALS", 
+        "RENDER_MODIFIERS",
+        "GLOBAL", 
+        "UNIQUE", 
+        "TWO_WAY_BINDING", 
+        "OTHER_DIRECTIVES", 
+        "OTHER_ATTR", 
+        "EVENTS", 
+        "CONTENT"
+      ]
+    }],
+    'vue/order-in-components':  ["error", {
+      "order": [
+        "el",
+        "name",
+        "parent",
+        "functional",
+        ["delimiters", "comments"],
+        ["components", "directives", "filters"],
+        "extends",
+        "mixins",
+        "inheritAttrs",
+        "model",
+        ["props", "propsData"],
+        "data",
+        "computed",
+        "watch",
+        "LIFECYCLE_HOOKS",
+        "methods",
+        ["template", "render"],
+        "renderError"
+      ]
+    }]
   },
   parserOptions: {
     parser: 'babel-eslint',
